dda10d10516142c0a9d8a4742a6c9917453293ab
rigui12138 <160600870+rigui12138@users.noreply.github.com>
2024-04-29 05:45:26 +0800

    Update wxPusher.js

--- wxPusher.js
+++ wxPusher.js
@@ -1,4 +1,4 @@
 module.exports = {
     appToken: process.env.WX_PUSHER_APP_TOKEN || 'AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr',
-    uid: process.env.WX_PUSHER_UID||''
+    uid: process.env.WX_PUSHER_UID||'UID_gSKGVCv3ZQXUJFjLWWrt80vkmySW'
   };

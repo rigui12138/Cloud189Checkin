a26a1efef9c094449a9e4c27a1e7573805b74d6a
rigui12138 <160600870+rigui12138@users.noreply.github.com>
2024-08-08 01:43:22 +0800

    Update telegramBot.js

--- telegramBot.js
+++ telegramBot.js
@@ -1,4 +1,4 @@
 module.exports = {
-  botToken: process.env.TELEGRAM_BOT_TOKEN || 'ysgx436764375s',
-  chatId: process.env.TELEGRAM_CHAT_ID || 'h5646752184',
+  botToken: process.env.TELEGRAM_BOT_TOKEN || 'ysgx4367nhyhgg64375s',
+  chatId: process.env.TELEGRAM_CHAT_ID || 'h56467jhghujj52184',
 };
